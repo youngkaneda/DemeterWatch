13a79356fe03c87e065a3cf8360990da14a7a9d8
Ricardo Job <sousajob@gmail.com>
2017-05-19 15:48:49 -0300

    Read files with NIO2. Three samples

--- parse-jdt/src/main/java/ifpb/gpes/jdt/main/ListagemDasChamadasDosMetodos.java
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/main/ListagemDasChamadasDosMetodos.java
@@ -3,8 +3,19 @@ package ifpb.gpes.jdt.main;
 import ifpb.gpes.jdt.MyAnotherVisitor;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -17,50 +28,80 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
  */
 public class ListagemDasChamadasDosMetodos {
 
-    public static void main(String[] args) {
+//    private static final String DIR_SRC = "C:/Users/Juan/Documents/NetBeansProjects/multiBds/atividade/src";
+    private static final String DIR_SRC = "/Users/job/Downloads/multi_bds/atividade/src";
+    private static final String DIR_PATH = System.getProperty("java.home") + "/lib/rt.jar";
 
+    public static void main(String[] args) {
         ASTParser parser = ASTParser.newParser(AST.JLS8);
-        String sourceDir = "C:/Users/Juan/Documents/NetBeansProjects/multiBds/atividade/src";
+//        TODO: Esses métodos podem/devem ser transferidos/usados em outra classe
+//        readFilePath(new File(DIR_SRC), parser);
+//        readFilesInPath(new File(DIR_SRC), parser);
+        walkFileTreeInPath(Paths.get(DIR_SRC), parser);
+    }
+    
+    private static void walkFileTreeInPath(Path get, ASTParser parser) {
+        try {
+            Files.walkFileTree(get, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    showMethods(file, parser);
+                    return super.visitFile(file, attrs);
+                }
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(ListagemDasChamadasDosMetodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void readFilesInPath(Path get, ASTParser parser) {
+        try {
+            Files.list(get).forEach((t) -> {
+                if (Files.isDirectory(t)) {
+                    readFilesInPath(t, parser);
+                } else {
+                    showMethods(t, parser);
+                }
+            });
+        } catch (IOException ex) {
+        }
+    }
+
+//    public static void readFilePath1(File file, ASTParser parser) {
+//        if (file.isDirectory()) {
+//            for (File listFile : file.listFiles()) {
+//                readFilePath(listFile, parser);
+//            }
+//        } else {
+//            showMethods(file, parser);
+//        }
+//    }
+    private static void showMethods(Path path, ASTParser parser) {
 
         try {
-            getFilePath(new File(sourceDir), parser);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+            String fileName = path.toFile().getName();
+            setConfig(fileName, parser);
 
-    }
+            MyAnotherVisitor visitor = new MyAnotherVisitor();
 
-    public static void getFilePath(File file, ASTParser parser) throws IOException {
-        if (file.isDirectory()) {
-            for (File listFile : file.listFiles()) {
-                getFilePath(listFile, parser);
+            byte[] readBytes = Files.readAllBytes(path);
+            String str = new String(readBytes);
+
+            parser.setSource(str.toCharArray());
+
+            CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+            if (cu.getAST().hasBindingsRecovery()) {
+                System.out.println("\nBinding activated.");
             }
-        } else {
-            showMethods(file, parser);
+
+            System.out.println("_" + fileName + "_\n");
+            cu.accept(visitor);
+
+            visitor.showLista();
+        } catch (IOException ex) {
+            Logger.getLogger(ListagemDasChamadasDosMetodos.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    private static void showMethods(File file, ASTParser parser) throws IOException {
-
-        setConfig(file.getName(), parser);
-
-        MyAnotherVisitor visitor = new MyAnotherVisitor();
-
-        byte[] readBytes = Files.readAllBytes(file.toPath());
-        String str = new String(readBytes);
-
-        parser.setSource(str.toCharArray());
-
-        CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
-        if (cu.getAST().hasBindingsRecovery()) {
-            System.out.println("\nBinding activated.");
-        }
-
-        System.out.println("_" + file.getName() + "_\n");
-        cu.accept(visitor);
-        
-        visitor.showLista();
     }
 
     public static void setConfig(String name, ASTParser parser) {
@@ -73,9 +114,10 @@ public class ListagemDasChamadasDosMetodos {
         parser.setCompilerOptions(options);
 
         parser.setUnitName(name);
-        String[] sources = {"C:/Users/Juan/Documents/NetBeansProjects/multiBds/atividade/src"};
-        String[] classpath = {System.getProperty("java.home") + "/lib/rt.jar"};
-        
+
+        String[] sources = {DIR_SRC};
+        String[] classpath = {DIR_PATH};
+
         parser.setEnvironment(classpath, sources, new String[]{"UTF-8"}, true);
     }
 
