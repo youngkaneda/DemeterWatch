54bb60270b6d12e43a2e20fa73a18b86111bd221
Ricardo Job <sousajob@gmail.com>
2017-05-24 23:24:58 -0300

    organizando a saída das informações

--- parse-jdt/src/main/java/ifpb/gpes/jdt/MyAnotherVisitor.java
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/MyAnotherVisitor.java
@@ -15,30 +15,59 @@ public class MyAnotherVisitor extends ASTVisitor {
 
     private static List<No> metodos = new ArrayList<>();
     private static int count = 0;
-    
+    private StringBuilder builder;
+
+    public MyAnotherVisitor() {
+        this("Not Class");
+    }
+
+    public MyAnotherVisitor(String fileName) {
+        this.builder = new StringBuilder("class:");
+        this.builder.append(fileName).append("\n");
+    }
+
     @Override
     public boolean visit(MethodInvocation mi) {
         IMethodBinding imb = mi.resolveMethodBinding();
-        System.out.println("\twho invoked "  + mi.getName() + "? " + mi.getExpression());
+//        System.out.println("\twho invoked "  + mi.getName() + "? " + mi.getExpression());
+        this.builder
+                .append("\twho invoked ")
+                .append(mi.getName())
+                .append("? ")
+                .append(mi.getExpression())
+                .append("\n");
+
         if (imb != null) {
             infos(imb);
             resolveInvocation(mi);
         } else {
-            System.out.println("\t" + mi.getName() + " - erro");
+            this.builder
+                    .append("\t")
+                    .append(mi.getName())
+                    .append(" - erro")
+                    .append("\n");
         }
-        
+
         return super.visit(mi);
     }
 
     private void infos(IMethodBinding imb) {
-        System.out.println("\twho invoked type " + imb.getDeclaringClass().getQualifiedName());
-        System.out.println("\tname " + imb.getName());
-        System.out.println("\treturn type " + imb.getReturnType().getQualifiedName());
+        this.builder
+                .append("\twho invoked type ")
+                .append(imb.getDeclaringClass().getQualifiedName())
+                .append("\tname ")
+                .append(imb.getName())
+                .append("\treturn type ")
+                .append(imb.getReturnType().getQualifiedName())
+                .append("\n");
     }
 
     @Override
     public boolean visit(MethodDeclaration md) {
-        System.out.println("md: " + md.getName());
+        this.builder
+                .append("md: ")
+                .append(md.getName())
+                .append("\n");
         IMethodBinding resolveMethodBinding = md.resolveBinding();
         resolveDeclaration(md);
 //        infos(resolveMethodBinding);
@@ -48,28 +77,33 @@ public class MyAnotherVisitor extends ASTVisitor {
     private static void resolveInvocation(MethodInvocation mi) {
         String nome = mi.getName().getFullyQualifiedName();
         String returnType = mi.resolveMethodBinding().getReturnType().getQualifiedName();
-        
+
         No no = new No(nome, returnType);
-        
-        metodos.get(count-1).addMethod(new Method(nome, returnType));
-    }
-    
-    private static void resolveDeclaration(MethodDeclaration md) {
-        if(count != 0)
-            count++;
-        
-        String nome = md.getName().getFullyQualifiedName();
-        String returnType = md.resolveBinding().getReturnType().getQualifiedName();
-        
-        No no = new No(nome, returnType);
-        
-        metodos.add(no);
-        
-        if(count == 0)
-            count++;
+
+        metodos.get(count - 1).addMethod(new Method(nome, returnType));
     }
 
-    public void showLista(){
-        System.out.println(metodos.toString());
+    private static void resolveDeclaration(MethodDeclaration md) {
+        if (count != 0) {
+            count++;
+        }
+
+        String nome = md.getName().getFullyQualifiedName();
+        String returnType = md.resolveBinding().getReturnType().getQualifiedName();
+
+        No no = new No(nome, returnType);
+
+        metodos.add(no);
+
+        if (count == 0) {
+            count++;
+        }
+    }
+
+    public void showLista() {
+        this.builder
+                .append(metodos.toString())
+                .append("\n");
+        System.out.println(this.builder.toString());
     }
 }
--- parse-jdt/src/main/java/ifpb/gpes/jdt/main/ListagemDasChamadasDosMetodos.java
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/main/ListagemDasChamadasDosMetodos.java
@@ -1,21 +1,16 @@
 package ifpb.gpes.jdt.main;
 
 import ifpb.gpes.jdt.MyAnotherVisitor;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -28,8 +23,8 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
  */
 public class ListagemDasChamadasDosMetodos {
 
-    private static final String DIR_SRC = "C:/Users/Juan/Documents/NetBeansProjects/multiBds/atividade/src";
-//    private static final String DIR_SRC = "/Users/job/Downloads/multi_bds/atividade/src";
+//    private static final String DIR_SRC = "C:/Users/Juan/Documents/NetBeansProjects/multiBds/atividade/src";
+    private static final String DIR_SRC = "/Users/job/Downloads/multi_bds/atividade/src";
     private static final String DIR_PATH = System.getProperty("java.home") + "/lib/rt.jar";
 
     public static void main(String[] args) {
@@ -82,7 +77,7 @@ public class ListagemDasChamadasDosMetodos {
             String fileName = path.toFile().getName();
             setConfig(fileName, parser);
 
-            MyAnotherVisitor visitor = new MyAnotherVisitor();
+            
 
             byte[] readBytes = Files.readAllBytes(path);
             String str = new String(readBytes);
@@ -95,10 +90,12 @@ public class ListagemDasChamadasDosMetodos {
                 System.out.println("\nBinding activated.");
             }
 
-            System.out.println("_" + fileName + "_\n");
+//            System.out.println("_" + fileName + "_\n");
+            MyAnotherVisitor visitor = new MyAnotherVisitor(fileName);
             cu.accept(visitor);
 
             visitor.showLista();
+            
         } catch (IOException ex) {
             Logger.getLogger(ListagemDasChamadasDosMetodos.class.getName()).log(Level.SEVERE, null, ex);
         }
