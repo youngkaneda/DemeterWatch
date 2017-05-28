301cf16e9fee2a633ba054a028c2b5264a2ebe85
Juan Pablo <juanpablolvl99@gmail.com>
2017-05-28 14:07:08 -0300

    informacoes extraidas

--- parse-jdt/src/main/java/ifpb/gpes/jdt/No.java
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/No.java
@@ -1,28 +1,77 @@
 package ifpb.gpes.jdt;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author Juan
  */
 class No {
-    private String nome;
-    private String returnType;
-    private List<Method> invocacoes;
-    
-    public No(String nome, String returnType){
-        this.nome = nome;
-        this.returnType = returnType;
-        this.invocacoes = new ArrayList<>();
+    private String a;
+    private String m;
+    private String rt;
+    private String c;
+    private String m1;
+    private String mi;
+    private String inv;
+
+    public String getInv() {
+        return inv;
+    }
+
+    public void setInv(String inv) {
+        this.inv = inv;
     }
     
-    public boolean addMethod(Method method){
-        return this.invocacoes.add(method);
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
+    }
+
+    public String getRt() {
+        return rt;
+    }
+
+    public void setRt(String rt) {
+        this.rt = rt;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getM1() {
+        return m1;
+    }
+
+    public void setM1(String m1) {
+        this.m1 = m1;
+    }
+
+    public String getMi() {
+        return mi;
+    }
+
+    public void setMi(String mi) {
+        this.mi = mi;
     }
 
     @Override
     public String toString() {
-        return "No{" + "nome=" + nome + ", returnType=" + returnType + ", invocacoes=" + invocacoes + '}';
+        return "No{" + "a=" + a + ", m=" + m + ", rt=" + rt + ", c=" + c + ", m1=" + m1 + ", mi=" + mi + ", inv=" + inv + '}';
     }
+    
 }
---
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/samples/X.java
@@ -0,0 +1,14 @@
+package ifpb.gpes.jdt.samples;
+
+/**
+ *
+ * @author juan
+ */
+public class X {
+    
+    private D d = new D();
+    public void testeOutro() {
+        d.teste();
+    }
+    
+}
