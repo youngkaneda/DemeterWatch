0a026b442fe225db7dace27c755860ec93b1b8fc
juan <juanpablolvl99@gmail.com>
2017-06-02 09:48:56 -0300

    parametros adicionados na saida dos metodos

--- parse-jdt/src/main/java/ifpb/gpes/jdt/MyAnotherVisitor.java
+++ parse-jdt/src/main/java/ifpb/gpes/jdt/MyAnotherVisitor.java
@@ -5,18 +5,16 @@ import java.util.List;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
-/**
- *
- * @author Juan
- */
+// JDR
+
 public class MyAnotherVisitor extends ASTVisitor {
 
-    private No n;
+    private No no;
     private static List<No> ns = new ArrayList<>();
-//    private static Set<No> ns = new HashSet<>();
     private static int count = 0;
 
     @Override
@@ -28,46 +26,89 @@ public class MyAnotherVisitor extends ASTVisitor {
                             @Override
                             public boolean visit(MethodInvocation mi) {
 
-                                n = new No();
-
-                                IMethodBinding imb = mi.resolveMethodBinding();
+                                no = new No();
 
                                 String a = "SAD";
                                 String returnType = "SADNESS";
+
+                                IMethodBinding imb = mi.resolveMethodBinding();
+
+                                ITypeBinding[] bindings = {};
+                                int length = 0;
+
                                 if (imb != null) {
+                                    bindings = imb.getParameterTypes();
+                                    length = bindings.length;
+
                                     a = imb.getDeclaringClass().getBinaryName();
                                     returnType = imb.getReturnType().getQualifiedName();
                                 }
-                                n.setA(a);
-                                String m = mi.getName().getIdentifier();
-                                n.setM(m);
-                                n.setRt(returnType);
+
+                                no.setA(a);
+
+                                StringBuilder m = new StringBuilder();
+                                m.append(mi.getName().toString()).append("[");
+
+                                for (int i = 0; i < length; i++) {
+                                    if (i < length - 1) {
+                                        m.append(bindings[i].getQualifiedName()).append(",");
+                                    } else {
+                                        m.append(bindings[i].getQualifiedName());
+                                    }
+                                }
+
+                                m.append("]");
+
+                                no.setM(m.toString());
+
+                                no.setRt(returnType);
+
                                 String c = md.resolveBinding().getDeclaringClass().getQualifiedName();
-                                n.setC(c);
-                                String m1 = md.getName().getIdentifier();
-                                n.setM1(m1);
+
+                                no.setC(c);
+
+                                StringBuilder m1 = new StringBuilder();
+                                m1 = m1.append(md.getName().getIdentifier()).append("[");
+
+                                bindings = md.resolveBinding().getParameterTypes();
+                                length = bindings.length;
+
+                                for (int i = 0; i < length; i++) {
+                                    if (i < length - 1) {
+                                        m1.append(bindings[i].getBinaryName()).append(",");
+                                    } else {
+                                        m1.append(bindings[i].getBinaryName());
+                                    }
+                                }
+
+                                m1.append("]");
+
+                                no.setM1(m1.toString());
+
                                 Expression inv = mi.getExpression();
 
                                 if (inv != null) {
                                     String[] ms = inv.toString().split("\\.");
                                     int size = ms.length;
 
-                                    n.setInv(ms[size - 1]);
+                                    no.setInv(ms[size - 1]);
                                 } else {
-                                    n.setInv("nothing here");
+                                    no.setInv("nothing here");
                                 }
 
-                                ns.add(n);
+                                ns.add(no);
                                 count++;
 
+                                String[] values = m.toString().split("\\[");
+
                                 if (count > 1) {
-                                    if (ns.get(count - 2).getInv().contains(m)) {
+                                    if (ns.get(count - 2).getInv().contains(values[0])) {
                                         ns.get(count - 1).setMi(ns.get(count - 2).getM());
                                     }
                                 }
 
                                 System.out.println("--");
-                                ns.stream().filter(t -> t.getMi() != null).forEach(System.out::println);
+                                ns.stream().forEach(System.out::println);
 
                                 return super.visit(mi);
                             }
